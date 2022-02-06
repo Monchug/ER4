e8c96bb5fd7cab2dd0d3fcbaad8539366011f22a
Eray Aynacı <73905858+Monchug@users.noreply.github.com>
2022-02-06 16:52:08 +0300

    Update er4.py

--- er4.py
+++ er4.py
@@ -1,10 +1,13 @@
 import discord
 from discord.ext import commands
 from discord.utils import get
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 client = commands.Bot("!")
-
-Token = ""
+Token = "OTIyODYwODE2MDc0NjkwNTcw.YcHnHA.pwCkCkNCt5lv5ow3EkOQzD9kkFM"
+username = "b201200007"
+password = "s5f5M7az"
 
 @client.event
 async def on_ready():  
@@ -25,5 +28,22 @@ async def on_message(ctx):
     await ctx.message.add_reaction("🇨")
     await ctx.message.add_reaction("🇩")
     await ctx.message.add_reaction("🇪")
+@client.command(name="bsmg")
+async def on_message(ctx):
+    if client.user == ctx.message.author:
+        return
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    driver = webdriver.Chrome(options=options)
+    driver.get("http://obs.sabis.sakarya.edu.tr/Ders/Grup/655940")
+    driver.find_element(By.NAME,"Username").send_keys(username)
+    driver.find_element(By.NAME,"Password").send_keys(password)
+    driver.find_element(By.NAME,"button").click()
 
+    duyuru = driver.find_element(By.CLASS_NAME,"timeline-items")
+    print(duyuru.text)
+    
+    await ctx.message.channel.send(duyuru.text[0:2000])
+    await ctx.message.channel.send(duyuru.text[2000:])
+    driver.quit()
 client.run(Token)
